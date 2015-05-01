280e69ac1742ad61a5bdde6b56d007146e188664
Katie Hoffman <ktahoffman@gmail.com>
2015-05-01 12:45:21 -0400

    add solution

--- spec/hello.spec.js
+++ spec/hello.spec.js
@@ -4,7 +4,7 @@ describe('hello', function() {
   it('logs "Hello World" to the screen', function() {
     spyOn(console, 'log');
     // v Your code below v
-
+    console.log("Hello World");
     // ^ Your code above ^
     expect(console.log).toHaveBeenCalledWith("Hello World")
   });
